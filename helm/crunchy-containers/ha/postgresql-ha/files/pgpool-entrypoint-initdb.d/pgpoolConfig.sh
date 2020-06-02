8d51cfeb4682f7d563fa929444c16213b1a7470b
jlanton <ext.jose.luis.anton@altia.es>
2020-06-02 14:00:25 +0200

    ADDED postgres deployment in High Availability configuration: Includes PgPool deployment and configuration, PostgreSql deployment and configuration and set up of all the users, databases creation and Metabase initialization (only structures, not data)

---
+++ helm/crunchy-containers/ha/postgresql-ha/files/pgpool-entrypoint-initdb.d/pgpoolConfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'testuser:md599e8713364988502fa6189781bcf648f' >> /opt/bitnami/pgpool/conf/pool_passwd
+echo 'dataflow:md5928cb8f47a17c110ec3836fcbabc976b' >> /opt/bitnami/pgpool/conf/pool_passwd
+echo 'dataset:md5b48e53545a60e28f455b798e47fd3bdb' >> /opt/bitnami/pgpool/conf/pool_passwd
+echo 'validation:md51ca3e86a924c07264cf43c0f2f00ad5c' >> /opt/bitnami/pgpool/conf/pool_passwd
+echo 'recordstore:md5756b7ae2393844e708eba18629818472' >> /opt/bitnami/pgpool/conf/pool_passwd
+sed -i "s/listen_backlog_multiplier = 2/listen_backlog_multiplier = 3/" /opt/bitnami/pgpool/conf/pgpool.conf
