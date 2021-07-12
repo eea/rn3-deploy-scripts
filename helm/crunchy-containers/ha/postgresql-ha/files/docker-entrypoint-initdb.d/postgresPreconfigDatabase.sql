1ae01e123989d9098acb12108f4b10805165ab1b
jlanton <ext.jose.luis.anton@altia.es>
2021-07-12 07:39:58 +0200

    cleaned end of lines

--- helm/crunchy-containers/ha/postgresql-ha/files/docker-entrypoint-initdb.d/postgresPreconfigDatabase.sql
+++ helm/crunchy-containers/ha/postgresql-ha/files/docker-entrypoint-initdb.d/postgresPreconfigDatabase.sql
@@ -1,12 +1,12 @@
-ALTER SYSTEM SET max_connections = '500';  
-ALTER SYSTEM SET shared_buffers = '1GB'; 
-ALTER SYSTEM SET maintenance_work_mem = '256MB'; 
+ALTER SYSTEM SET max_connections = '500';
+ALTER SYSTEM SET shared_buffers = '1GB';
+ALTER SYSTEM SET maintenance_work_mem = '256MB';
 ALTER SYSTEM SET work_mem = '64MB';
 ALTER SYSTEM SET checkpoint_completion_target = '0.9';
 ALTER SYSTEM SET random_page_cost = '1.1';
 ALTER SYSTEM SET effective_io_concurrency = '300';
-ALTER SYSTEM SET min_wal_size = '1GB'; 
-ALTER SYSTEM SET max_wal_size = '4GB'; 
+ALTER SYSTEM SET min_wal_size = '1GB';
+ALTER SYSTEM SET max_wal_size = '4GB';
 ALTER SYSTEM SET synchronous_commit='remote_apply';
 
 
@@ -25,23 +25,23 @@ DROP DATABASE IF EXISTS metabase;
 DROP DATABASE IF EXISTS keycloak;
 
 CREATE DATABASE datasets
-    WITH 
+    WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     TEMPLATE = template0
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
-	
+
 CREATE DATABASE metabase
-    WITH 
+    WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     TEMPLATE = template0
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
-	
+
 CREATE DATABASE keycloak
-    WITH 
+    WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     TEMPLATE = template0
