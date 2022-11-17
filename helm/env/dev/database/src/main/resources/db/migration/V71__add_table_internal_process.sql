0764389a0b40c6c0765521fb6181fbb3d15e3179
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2022-11-17 17:07:21 +0200

    Update V71__add_table_internal_process.sql

--- helm/env/dev/database/src/main/resources/db/migration/V71__add_table_internal_process.sql
+++ helm/env/dev/database/src/main/resources/db/migration/V71__add_table_internal_process.sql
@@ -8,4 +8,12 @@ transaction_id varchar NOT NULL,
 aggregate_id varchar NOT NULL,
 CONSTRAINT internal_process_pk PRIMARY KEY (id)
 );
+CREATE SEQUENCE IF NOT EXISTS public.internal_process_id_seq
+  INCREMENT BY 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1
+  NO CYCLE;
 GRANT ALL ON TABLE public.internal_process TO testuser,dataflow,dataset,validation,recordstore;
+GRANT ALL ON SEQUENCE public.internal_process_id_seq TO testuser,dataflow,dataset,validation,recordstore;
