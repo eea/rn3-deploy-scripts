0202d229ab57061ea2ae0684565ea1446f395e1f
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2022-12-15 15:39:52 +0200

    Update V39__create_data_provider_group_table.sql

--- helm/eaa-deploy/database/src/main/resources/db/migration/V39__create_data_provider_group_table.sql
+++ helm/eaa-deploy/database/src/main/resources/db/migration/V39__create_data_provider_group_table.sql
@@ -1,8 +1,10 @@
-CREATE TABLE public.data_provider_group (
-	id int8 NOT NULL,
-	"name" varchar(255) NULL,
-	"type" varchar(255) NULL,
-	CONSTRAINT data_provider_gp_pk PRIMARY KEY (id)
-) WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = ‘data_provider_group’)\gexec;
+CREATE TABLE IF NOT EXISTS public.data_provider_group (
+    id int8 NOT NULL,
+    "name" varchar(255) NULL,
+    "type" varchar(255) NULL,
+    CONSTRAINT data_provider_gp_pk PRIMARY KEY (id)
+);
+
+ 
 
 GRANT ALL ON TABLE public.data_provider_group TO testuser, dataflow, dataset, validation, recordstore;
