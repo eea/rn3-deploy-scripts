58a5ad8326ff37a93bb2dc5e5c3fafdde4c23493
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2023-01-04 17:23:13 +0200

    cleanup

---
+++ helm/eaa-deploy/scripts/consul_cleaner/sandbox_env/cleanup_consul.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Trasys.gr Clear Consul services  by Tavoulas Sotiris November/2022
+#
+# LAST UPDATE @ 11/11/2022 Tavoulas Sotiris / Sotirios.TAVOULAS-KAVOULAS@trasys.gr
+# 
+
+tmp_fld="./data"
+services=( 
+"apiGateway"
+"collaboration"
+"communication"
+"orchestrator"
+"dataflow"
+"dataset"
+"document"
+"recordstore"
+"rod"
+"ums"
+"validation"
+)
+
+for (( i=0; i<${#services[@]}; i++ ));
+do
+		curl http://kvm-rn3stg-01.pdmz.eea:30741/v1/catalog/service/${services[$i]}| jq . > $tmp_fld/${services[$i]}.json 
+		cat $tmp_fld/${services[$i]}.json  |grep '"ServiceID":' > $tmp_fld/${services[$i]}.txt 
+		cat $tmp_fld/${services[$i]}.txt | awk -F": " '{print $2}' > $tmp_fld/AWK_${services[$i]}.txt
+		sed -i 's/"//g' $tmp_fld/AWK_${services[$i]}.txt 
+		sed -i 's/,//g' $tmp_fld/AWK_${services[$i]}.txt
+		filename="$tmp_fld/AWK_${services[$i]}.txt"
+
+		cat $filename
+
+		echo Start
+		while read p; do 
+			#sleep 2
+			echo "Removing $p from  http://kvm-rn3stg-01.pdmz.eea:30741 "
+			curl -v -X PUT http://kvm-rn3stg-01.pdmz.eea:30741/v1/agent/service/deregister/$p
+			#sleep 1
+			#echo "Removing $p from the second endpoint http://10.50.4.185:32233 just for the fun of it !!!!"
+			#curl -v -X PUT http://10.50.5.14:32233/v1/agent/service/deregister/$p
+			
+		done < "$filename"
+done
+
+
+
