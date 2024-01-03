c837860867851ac74a59b4b009e84a6faf259806
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2024-01-03 17:10:16 +0200

    Update secrets.groovy

--- secrets.groovy
+++ secrets.groovy
@@ -29,10 +29,12 @@ def CopySecretFiles()
 						// FOR EACH ENV WE NEED TO PUT THE KEYCLOAK KEY TO THE BELOW BLOCK - WHENEVER THERE IS A NEW KEY !
 
 
-						    if ("${params.Env}" == "dev") { 
-								echo -n  "changing properties secret file to $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties THE KEYCLOAK_SECRET"
-								sh  "sed -i 's/3c03f809-d830-4af8-9cfe-9471297b3b4a/3c03f809-d830-4af8-9cfe-9471297b3b4a-TEST/g' $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
-								}
+						if ( ${params.Env} == "dev") 
+						{ 
+							echo -n  "changing properties secret file to $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties THE KEYCLOAK_SECRET"
+							sh  "sed -i 's/3c03f809-d830-4af8-9cfe-9471297b3b4a/3c03f809-d830-4af8-9cfe-9471297b3b4a-TEST/g' $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
+							;;
+						}
 
 						// sh "case ${params.Env} in
 						// 	dev)
