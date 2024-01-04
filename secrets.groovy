f40104cbedea20cb1e1bec2ab0aa6a86fce3bdbf
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2024-01-04 14:39:11 +0200

    Update secrets.groovy

--- secrets.groovy
+++ secrets.groovy
@@ -7,39 +7,19 @@ def CopySecretFiles()
 					sh "echo ######################################################################################################################"
 					 //copy the secret file to destination
 					withCredentials([file(credentialsId: 'application.properties', variable: 'FILE')]) {
-						//sh "rm  $WORKSPACE/helm/eaa-deploy/application-config/files/application*"
+
 						echo "writing properties secret file to $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
 						sh "cp \$FILE $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
 						sh "sed -i 's/dev.reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g' $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
-						//sh "ls -la  $WORKSPACE/helm/eaa-deploy/application-config/files/"
-						//sh "cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties"
 
-						// Changes on 07122023 cause of test failing to connect to keycloak !!!!
-						//KEYCLOAK_REDIRECT_URI
-						// from 
-						// config/application/eea.keycloak.redirect_uri=${KEYCLOAK_REDIRECT_URI:https://reportnet.europa.eu/eulogin/}
-						// to
-						// config/application/eea.keycloak.redirect_uri=${KEYCLOAK_REDIRECT_URI:https://test.reportnet.europa.eu/eulogin/}}
-						// and for keycloak key in test ENV 
-						// config/application/eea.keycloak.secret=${KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24}
-						// to 
-						// config/application/eea.keycloak.secret=${KEYCLOAK_SECRET:39938213-b771-4ab3-a5ed-c525e88026b7}
-
-
-// Dev PubKey
-// from
-//from 
-// KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB}
-// KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB}
+						
 						// FOR EACH ENV WE NEED TO PUT THE NEW KEYCLOAK KEY TO THE BELOW BLOCK - WHENEVER THERE IS A NEW KEY !
-
 						 def KC_OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"  //local variable
 						 def DEV_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"  //local variable
 						 def TEST_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB" //local variable
-						 def STAGING_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:CHANGEME" //local variable
+						 def SANDBOX_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB" //local variable
 						 def PROD_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:CHANGEME" //local variable
 						 def PROD2_KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:CHANGEME" //local variable
-
 
 						 sh """	case "${params.Env}" in
 								dev )
@@ -48,19 +28,19 @@ def CopySecretFiles()
 									sed -i "s|reportnet.europa.eu|${params.Env}.reportnet.europa.eu|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
 								test ) 
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_CLIENT_PUBLIC_KEY"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_CLIENT_PUBLIC_KEY"
 									sed -i "s|${KC_OLD_PUB_KEY}|${TEST_KC_NEW_PUB_KEY}|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s|KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24|KEYCLOAK_SECRET:cd7691a4-9f26-48a2-be9a-4d4dbde2b331|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s|reportnet.europa.eu|${params.Env}.reportnet.europa.eu|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
-									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_CLIENT_PUBLIC_KEY"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
+									#cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_CLIENT_PUBLIC_KEY"
 									;;
-								sandbox | testing )
-									sed -i "s|${KC_OLD_PUB_KEY}|${STAGING_KC_NEW_PUB_KEY}|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
-									sed -i "s|KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24|KEYCLOAK_SECRET:~~~~~~~~~~~CHANGE ME ~~~~~~~~~~~~|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
+								sandbox )
+									sed -i "s|${KC_OLD_PUB_KEY}|${SANDBOX_KC_NEW_PUB_KEY}|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
+									sed -i "s|KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24|KEYCLOAK_SECRET:24c0724e-6f20-48f6-917f-e83ef5f03926|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s|reportnet.europa.eu|${params.Env}.reportnet.europa.eu|g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
 								prod )
@@ -75,13 +55,6 @@ def CopySecretFiles()
 									;;
 								esac
 							"""
-
-
-
-
-
-
-
 					}	
 
 
