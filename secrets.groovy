20e1f25717c1af9ea123814cc6c9334e772c98b3
Tavoulas Sotiris <Sotirios.TAVOULAS-KAVOULAS@trasys.gr>
2024-01-04 12:23:18 +0200

    Update secrets.groovy

--- secrets.groovy
+++ secrets.groovy
@@ -26,33 +26,52 @@ def CopySecretFiles()
 						// config/application/eea.keycloak.secret=${KEYCLOAK_SECRET:39938213-b771-4ab3-a5ed-c525e88026b7}
 
 
+// Dev PubKey
+// from
+//from 
+// KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB}
+// KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB}
 						// FOR EACH ENV WE NEED TO PUT THE NEW KEYCLOAK KEY TO THE BELOW BLOCK - WHENEVER THERE IS A NEW KEY !
 						 sh """	case "${params.Env}" in
 								dev )
+									export OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"
+									export NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB"
 									echo "dev Environment was selected" 
 									echo "The contents of the application.properties file is currently : " 
 									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
 									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
 									echo "Changing the file  " 
+									sed -i "s/KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB/KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
+									
 									sed -i "s/KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24/KEYCLOAK_SECRET:8bb193b1-fc5e-4112-9c4c-58a0d615c7e8/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
+									
 									echo "The contents of application.properties file now is  : "
 									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_SECRET"
 									cat $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties | grep "KEYCLOAK_REDIRECT_URI"
 									;;
 								test ) 
+									export KC_OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"
+									export KC_NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgbMIQ/yJEarq3gz5Ie+wqXs6p878xDeZcM/u8zu/f950BLAcNoNrXTt6lyFThhqxnD4O2N6Ffz4TIOigNwHsXbVGPZy2N1o8Smdaxk+YvrbzOXFELYEna2CZtwV6Gl7nkoLjmZVS143QunYLJ3d34ZTRskp5CYrFJRjaCBnB5LXGilAzaEdLRb4Rr6jU9xker7HGOx4/ZWeNkE3IwLkCzkKeGO8Jz7HS+xzwaMuYXCIl/8WD8e0fcKb6RzruDfepGtQmoVtmmzLF+3kQJOHN0vu+QeYkF7mLkkBAbXDbEgihILnOyocF1S+pEsxaHPkiVeslH32ieV1NWbqf7t3UPwIDAQAB"
+									sed -i "s/$KC_OLD_PUB_KEY/$KC_NEW_PUB_KEY/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24/KEYCLOAK_SECRET:cd7691a4-9f26-48a2-be9a-4d4dbde2b331/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
-								sandbox | testing ) 
+								sandbox | testing )
+									export OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"
+									export NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:~~~~~~~~~~~~~~~~~ CHANGE ME ~~~~~~~~~~~~~~~~~~~~~"
 									sed -i "s/KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24/KEYCLOAK_SECRET:24c0724e-6f20-48f6-917f-e83ef5f03926/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
-								prod ) 
+								prod )
+									export OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"
+									export NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:~~~~~~~~~~~~~~~~~ CHANGE ME ~~~~~~~~~~~~~~~~~~~~~"
 									sed -i "s/KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24/KEYCLOAK_SECRET: ~~~~~ CHANGE ME ~~~~~~ /g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
-								prod2 | transport ) 
+								prod2 | transport )
+									export OLD_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuvOUY9g2UITSyRMk4yNnA0sgzWmjKdRFNHGixZRxDzyjUjd6jrgdTYcmC7NXRZK9+BJujOMd9ArjECVkKiReKudmg7l/zEWfB60EM+XDP3aqox2/v3ytwoasCQJF0MmoiljrSChHLygNJmEaOY++3UcXbw+OwTBK2C9LQNG1X3U0xv6tPNXSqq0OUtFka9Otjujam/8hrqurPOW3tWPfaMvh5gw07CBhe+YnIqmTI/LuJxjIf0Q7tbE2HD8TJLeS8LqySHqXlZPQ9tDiqZtkm8e3jGs2U7ZgHy3DNLz1xB5M8rBKA2/3bI+FIfY6dg/M4uPN4LgPxm5NXOpdA2k34wIDAQAB"
+									export NEW_PUB_KEY="KEYCLOAK_CLIENT_PUBLIC_KEY:~~~~~~~~~~~~~~~~~ CHANGE ME ~~~~~~~~~~~~~~~~~~~~~"
 									sed -i "s/KEYCLOAK_SECRET:0380996f-a7ad-4667-8ba4-14995e408d24/KEYCLOAK_SECRET: ~~~~~ CHANGE ME ~~~~~~ /g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									sed -i "s/reportnet.europa.eu/${params.Env}.reportnet.europa.eu/g" $WORKSPACE/helm/eaa-deploy/application-config/files/application.properties
 									;;
